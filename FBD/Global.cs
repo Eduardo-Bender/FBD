ebbabb3794689d34813eefa3fcd180bd1e3381cc
Eduardo-Bender <103793216+Eduardo-Bender@users.noreply.github.com>
2023-12-02 19:50:17 -0300

    Atualizado e buildado
    Versão final

--- FBD/Global.cs
+++ FBD/Global.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Protobuf.Collections;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
 
 namespace FBD
 {
@@ -31,14 +35,14 @@ namespace FBD
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS clientes " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "nome char(40), " +
                                        "morada char(40), " +
                                        "telefone int(11))", Conexao);
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS funcionarios " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "nome char(40), " +
                                        "morada char(40), " +
                                        "telefone int(11), " +
@@ -46,13 +50,13 @@ namespace FBD
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS veiculos " +
-                                       "(id integer auto_increment primary key, " + 
+                                       "(id integer not null auto_increment primary key, " + 
                                        "id_cliente int, foreign key (id_cliente) references clientes(id), " +
                                        "data_aquisicao date)", Conexao);
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS pecas " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "designacao char(40), " +
                                        "custo_unitario int(5), " +
                                        "quantidade_armazem int(10))", Conexao);
@@ -67,7 +71,7 @@ namespace FBD
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS reparos " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "id_cliente int, foreign key (id_cliente) references clientes(id), " +
                                        "id_veiculo int, foreign key (id_veiculo) references veiculos(id), " +
                                        "trabalho int, foreign key (trabalho) references mao_de_obra(trabalho), " +
@@ -103,7 +107,19 @@ namespace FBD
                                    " order by trabalho desc", Conexao);
                 Comando.ExecuteNonQuery();
 
-            } catch (MySqlException ex)
+                MySqlCommand comandoMaoDeObraView = new MySqlCommand(
+                                    "CREATE VIEW bd_fbd.mao_de_obraView AS " +
+                                    "SELECT Mao_de_obra.trabalho, Funcionarios.Nome, Pecas.Designacao, Mao_de_obra.Tempo, Mao_de_obra.Custo " +
+                                    "FROM bd_fbd.Mao_de_obra " +
+                                    "INNER JOIN bd_fbd.Funcionarios ON Mao_de_obra.ID_Funcionario = Funcionarios.ID " +
+                                    "INNER JOIN bd_fbd.Pecas ON Mao_de_obra.ID_Peca = Pecas.ID " +
+                                    "ORDER BY Mao_de_obra.trabalho DESC;", Conexao);
+                comandoMaoDeObraView.ExecuteNonQuery();
+
+
+
+            }
+            catch (MySqlException ex)
             {
                 if(ex.Code == 1050)
                 {
@@ -112,14 +128,14 @@ namespace FBD
             }
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS HISTORICOclientes " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "nome char(40), " +
                                        "morada char(40), " +
                                        "telefone int(11))", Conexao);
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS HISTORICOfuncionarios " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "nome char(40), " +
                                        "morada char(40), " +
                                        "telefone int(11), " +
@@ -127,13 +143,13 @@ namespace FBD
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS HISTORICOveiculos " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "id_cliente int, foreign key (id_cliente) references HISTORICOclientes(id), " +
                                        "data_aquisicao date)", Conexao);
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS HISTORICOpecas " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "designacao char(40), " +
                                        "custo_unitario int(5), " +
                                        "quantidade_armazem int(10))", Conexao);
@@ -148,7 +164,7 @@ namespace FBD
             Comando.ExecuteNonQuery();
 
             Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS HISTORICOreparos " +
-                                       "(id integer auto_increment primary key, " +
+                                       "(id integer not null auto_increment primary key, " +
                                        "id_cliente int, foreign key (id_cliente) references HISTORICOclientes(id), " +
                                        "id_veiculo int, foreign key (id_veiculo) references HISTORICOveiculos(id), " +
                                        "trabalho int, foreign key (trabalho) references HISTORICOmao_de_obra(trabalho), " +
@@ -160,6 +176,51 @@ namespace FBD
                                        "\r\n\tINSERT INTO HISTORICOClientes (Nome, Morada, Telefone) \nVALUES (OLD.Nome, OLD.Morada, OLD.Telefone);" +
                                        "\r\n", Conexao);
             Comando.ExecuteNonQuery();
+
+            Comando = new MySqlCommand("CREATE TRIGGER IF NOT EXISTS before_delete_funcionarios2 " +
+                                       "BEFORE DELETE " +
+                                        "ON bd_fbd.HISTORICOfuncionarios " +
+                                        "FOR EACH ROW" +
+                                            " INSERT INTO HISTORICOfuncionarios(nome, morada, telefone, categoria)" +
+                                            " VALUES(OLD.nome, OLD.morada, OLD.telefone, OLD.categoria);", Conexao);
+            Comando.ExecuteNonQuery();
+
+            Comando = new MySqlCommand("CREATE TRIGGER IF NOT EXISTS before_delete_veiculos2 " +
+                                        "BEFORE DELETE " +
+                                        "ON bd_fbd.HISTORICOveiculos " +
+                                        "FOR EACH ROW" +
+                                        " INSERT INTO HISTORICOveiculos(id_cliente, data_aquisicao)" +
+                                        " VALUES(OLD.id_cliente, OLD.data_aquisicao);", Conexao);
+            Comando.ExecuteNonQuery();
+
+
+            MySqlCommand comandoPecas = new MySqlCommand(
+                                        "CREATE TRIGGER IF NOT EXISTS before_delete_pecas2 " +
+                                        "BEFORE DELETE " +
+                                        "ON bd_fbd.HISTORICOpecas " +
+                                        "FOR EACH ROW" +
+                                        " INSERT INTO HISTORICOpecas(designacao, custo_unitario, quantidade_armazem)" +
+                                        " VALUES(OLD.designacao, OLD.custo_unitario, OLD.quantidade_armazem);", Conexao);
+            comandoPecas.ExecuteNonQuery();
+
+
+            MySqlCommand comandoMaoDeObra = new MySqlCommand(
+                                        "CREATE TRIGGER IF NOT EXISTS before_delete_mao_de_obra2 " +
+                                        "BEFORE DELETE " +
+                                        "ON bd_fbd.HISTORICOmao_de_obra " +
+                                        "FOR EACH ROW" +
+                                        " INSERT INTO HISTORICOmao_de_obra(id_funcionario, id_peca, custo, tempo)" +
+                                        " VALUES(OLD.id_funcionario, OLD.id_peca, OLD.custo, OLD.tempo);", Conexao);
+            comandoMaoDeObra.ExecuteNonQuery();
+
+            MySqlCommand comandoReparos = new MySqlCommand(
+                                        "CREATE TRIGGER IF NOT EXISTS before_delete_reparos2 " +
+                                        "BEFORE DELETE " +
+                                        "ON bd_fbd.HISTORICOreparos " +
+                                        "FOR EACH ROW" +
+                                        " INSERT INTO HISTORICOreparos(id_cliente, id_veiculo, trabalho, data_reparo, custo_total)" +
+                                        " VALUES(OLD.id_cliente, OLD.id_veiculo, OLD.trabalho, OLD.data_reparo, OLD.custo_total);", Conexao);
+            comandoReparos.ExecuteNonQuery();
 
 
 
